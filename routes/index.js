1852a95df41187dea3d4336108c57856a97b59f4
Alessandro Mascheroni <maske94@gmail.com>
2017-03-11 22:18:35 +0100

    Change index.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ticino Team Server',
-                        subTitle: 'Pollution Problem'});
+                        subTitle: 'Work in progress...'});
 });
 
 module.exports = router;
